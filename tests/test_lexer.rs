384d2ad49e88ba5e244d43d2dc53ba4452b80b81
Obinna Ikeh <hikenike6@gmail.com>
2023-01-14 19:19:50 +0100

    move tests folder to project level

---
+++ tests/test_lexer.rs
@@ -0,0 +1,13 @@
+use beesafe::lexer::{Lexer, Token, TType};
+
+#[test]
+fn test_next_token() {
+   let input_string = String::from("declare x
+   declare y");
+   let mut lexer = Lexer::new(input_string);
+   let tokens = lexer.parse_tokens();
+   assert_eq!(tokens.get(0), Some(&Token::from("declare", TType::Declare)));
+   assert_eq!(tokens.get(1), Some(&Token::from("x", TType::Id)));
+   assert_eq!(tokens.get(2), Some(&Token::from("declare", TType::Declare)));
+   assert_eq!(tokens.get(3), Some(&Token::from("y", TType::Id)));
+}
