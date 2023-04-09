e0ea8193bd572261b2d5850fe30526150ec52a54
Obinna Ikeh <hikenike6@gmail.com>
2023-04-09 20:58:19 +0100

    [parser/lexer] add test + update test

--- tests/test_lexer.rs
+++ tests/test_lexer.rs
@@ -8,6 +8,7 @@ fn test_next_token() {
    let tokens = lexer.parse_tokens();
    assert_eq!(tokens.get(0), Some(&Token::from("declare", TType::Declare)));
    assert_eq!(tokens.get(1), Some(&Token::from("x", TType::Id)));
-   assert_eq!(tokens.get(2), Some(&Token::from("declare", TType::Declare)));
-   assert_eq!(tokens.get(3), Some(&Token::from("y", TType::Id)));
+   assert_eq!(tokens.get(2), Some(&Token::from("\n", TType::Newline)));
+   assert_eq!(tokens.get(3), Some(&Token::from("declare", TType::Declare)));
+   assert_eq!(tokens.get(4), Some(&Token::from("y", TType::Id)));
 }
---
+++ tests/test_parser.rs
@@ -0,0 +1,22 @@
+use beesafe::parser;
+use beesafe::lexer;
+
+#[test]
+fn test_expression(){
+    let input_string = String::from(r#"
+        1 + 2 - 4 
+        7 * 4 
+        9 - 0 
+        8 * 9 + 5 
+        3 + 4
+    "#);
+    let mut lexer = lexer::Lexer::new(input_string);
+    let mut parser = parser::Parser::new(&mut lexer);
+    let program = parser.parse_program();
+    let exp_results = [-1, 28, 9, 77, 7];
+    assert_eq!(program.statements.len(), 5);
+    for (i, node) in program.statements.iter().enumerate() {
+        assert_eq!(*node.visit().visit().downcast::<i32>().unwrap_or(Box::new(-1)), exp_results[i]);
+    }
+    assert_eq!(parser.has_errors(), false);
+}
