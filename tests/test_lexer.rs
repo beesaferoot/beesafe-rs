87f170b4dbcbdcadf229868b2b18b696589fda46
Obinna Ikeh <hikenike6@gmail.com>
2023-04-13 13:03:02 +0100

    [lexer] update test

--- tests/test_lexer.rs
+++ tests/test_lexer.rs
@@ -6,9 +6,9 @@ fn test_next_token() {
    declare y");
    let mut lexer = Lexer::new(input_string);
    let tokens = lexer.parse_tokens();
-   assert_eq!(tokens.get(0), Some(&Token::from("declare", TType::Declare)));
-   assert_eq!(tokens.get(1), Some(&Token::from("x", TType::Id)));
-   assert_eq!(tokens.get(2), Some(&Token::from("\n", TType::Newline)));
-   assert_eq!(tokens.get(3), Some(&Token::from("declare", TType::Declare)));
-   assert_eq!(tokens.get(4), Some(&Token::from("y", TType::Id)));
+   assert_eq!(tokens.get(0), Some(&Token::from("declare", TType::Declare, 0)));
+   assert_eq!(tokens.get(1), Some(&Token::from("x", TType::Id, 8)));
+   assert_eq!(tokens.get(2), Some(&Token::from("\n", TType::Newline, 9)));
+   assert_eq!(tokens.get(3), Some(&Token::from("declare", TType::Declare, 13)));
+   assert_eq!(tokens.get(4), Some(&Token::from("y", TType::Id, 21)));
 }
